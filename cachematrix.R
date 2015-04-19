d818300c2d776dffd306ee6ba09ed5ed91c2de5d
Ray D <rayxi2.71828@outlook.com>
2015-04-19 12:05:31 +0800

    Added documentation and rudimentary check in cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## In here, we're creating a function that returns a list containing
-## getter/setter for two things: the matrix itself, and its inverse.
-
 makeCacheMatrix <- function(my.matrix = matrix()) {
+  # Create a special kind of matrix, one that can cache its own inverse
+  #
+  # Args:
+  #   my.matrix: The "raw" matrix whose inverse we want to cache
+  #
+  # Returns:
+  #   A list containing four functions: getter/setter for the matrix, 
+  #   and the getter/setter of its inverse.
   my.matrix.inverse <- NULL
   set <- function(the.matrix) {
     my.matrix <<- the.matrix
@@ -28,6 +33,22 @@ makeCacheMatrix <- function(my.matrix = matrix()) {
 ## first. If yes, just return it right away. 
 
 cacheSolve <- function(the.cache.matrix, ...) {
+  # Given the special cache matrix created using the makeCacheMatrix
+  # function above, either return the cached inverse if it's been 
+  # calculated before, or calculate it and store it in the cache.
+  #
+  # Args:
+  #   the.cache.matrix: The special cache matrix whose inverse we want to get
+  #   ...: further arguments to pass to the solve method.
+  #
+  # Returns:
+  #   The inverse of the raw matrix contained within the cache matrix.
+  
+  # rudimentary check 
+  if(class(the.cache.matrix) != "list") {
+    stop("Please use makeCacheMatrix to wrap your matrix first")
+  }
+  
   ## Return a matrix that is the inverse of 'x'
   the.probably.cached.matrix.inverse <- the.cache.matrix$getMatrixInverse()
   if(!is.null(the.probably.cached.matrix.inverse)) {
